32d89f52fbb8939ab940e2d7301b261486b8cdd7
itsmesunky <itisp_06@naver.com>
2023-10-19 14:34:25 +0900

    ✨Feat: 로그인, 메인페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
+import Login from "./pages/Member/Login";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+          {/* 메인 페이지 */}
+          <Route path="/" element={<MainPage />} />
+
+          {/* 로그인 페이지 */}
+          <Route path="/login" element={<Login />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
