50367477517286bb2b884c88fe8904758e0664cf
itsmesunky <itisp_06@naver.com>
2023-10-19 17:58:06 +0900

    ✨Feat: 어드민 페이지 추가

---
+++ src/pages/Admin/UserManagement,.jsx
@@ -0,0 +1,14 @@
+/**
+ * 어드민 페이지 - 사용자 관리
+ * @Auth 
+ */
+
+import UserList from "../../components/Admin/UserList";
+
+export default function UserManagement() {
+    return (
+        <>
+            <UserList />
+        </>
+    )
+}
